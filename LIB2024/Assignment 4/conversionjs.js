ca836ba4098d067227c222a74c5981f273a00eef
mcna0303 <mcna0303@algonquinlive.com>
2023-04-03 17:29:16 -0400

    Thermometer Update

--- LIB2024/Assignment 4/conversionjs.js
+++ LIB2024/Assignment 4/conversionjs.js
@@ -1,4 +1,4 @@
-/* When the input field receives input, convert the value from fahrenheit to celsius */
+/* The below Javascript is for converting the temperature! */
 function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
   document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
